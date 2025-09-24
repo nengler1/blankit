c4a7f49e517e9d4e35188d08a29591e647d25a1e
Garrett S <gsmith17@uccs.edu>
2025-09-24 13:03:31 -0600

    Added TODOs for image upload and processing functionality

--- Front-End/window.py
+++ Front-End/window.py
@@ -22,10 +22,22 @@ root.geometry(f'{width}x{height}+{center_x}+{center_y}')
 def upload_photo():
     file_path = filedialog.askopenfilename()
     print(f"Selected File: {file_path}")
-    # Add logic to process the image file here
+    # TODO: Add logic to process the image file
+    #       Display the image in the window
+    #       Add functionality to edit images (blur, redact, crop, free color, paint bucket, etc.)
+    #       Add functionality to save images
+    #       Add functionality to remove metadata
+    #       Add functionality to undo/redo changes
 
 window = tk.Tk()
 window.title("Image Uploader")
 window.lift()
 window.geometry(f'{width//2}x{height//2}+{center_x*(3//2)}+{center_y*(3//2)}')
+# TODO:     Add image upload button of any filetype (png, jpeg, jpg, etc.)
+#           Add image display and confirmation
+#           Focus window until complete
+#           Close window when complete
+
 root.mainloop()
+
+
